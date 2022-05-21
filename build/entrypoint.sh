f06f55978d68a45d5915db0537156ba0d22a4a00
Michael Fieldness <mfieldness@gmail.com>
2022-05-20 18:25:32 -0700

    Fix config

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -5,11 +5,11 @@ cp /root/smb.conf /etc/samba/smb.conf
 echo "   interfaces = ${INTERFACES}" >> /etc/samba/smb.conf
 
 if [[ "${NETBIOS_NAME}" ]]; then
-    echo "   wins server = ${NETBIOS_NAME}" >> /etc/samba/smb.conf
+    echo "   netbios name = ${NETBIOS_NAME}" >> /etc/samba/smb.conf
 fi
 
 if [[ "${NETBIOS_ALIASES}" ]]; then
-    echo "   wins server = ${NETBIOS_ALIASES}" >> /etc/samba/smb.conf
+    echo "   netbios aliases = ${NETBIOS_ALIASES}" >> /etc/samba/smb.conf
 fi
 
 if [[ "${WINS_SERVER}" ]]; then
